cd635f14c18ef731da7da577c92263ab8bf67224
Yoga Bayu Aji Pranawa <khususlain@yahoo.com>
2016-05-05 00:59:08 +0800

    editing search view

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,11 +4,45 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
     @if(empty($iklan))
-      <h1>Tidak ditemukan</h1>
+      <div class="col-md-12">
+         <h2>Pencarian Tidak Ditemukan</h2>
+       </div>
     @else
-      <div class="panel panel-info">        
-        <div class="panel-heading">Hasil Pencarian</div>
+      @foreach($iklan as $post)
+        <div class="col-md-4">
+          <div class="panel panel-primary">
+                <div class="panel-heading">
+                  <a href="{{URL::to('iklan_detail')}}/{{$post->id_iklan}}" class="panel-title">{{$post->judul_iklan}}</a>
+                </div>
+                <div class="panel-body">
+                  <table class="table table-hover table-stripped">
+                    <tr>
+                      <td>Harga(kg)</td>
+                      <td>{{$post->harga}}</td>
+                    </tr>
+                    <tr>
+                      <td>Deskripsi</td>
+                      <td>{{$post->deskripsi_iklan}}</td>
+                    </tr>
+                    <tr>
+                      <td>Stok(kg)</td>
+                      <td>{{$post->stok}}</td>
+                    </tr>
+                    <tr>
+                      <td>Penjual</td>
+                      <td>{{$post->nama_user}}</td>
+                    </tr>
+                    <tr>
+                      <td>Gambar</td>
+                      <td><img src="{{URL::to($post->gambar)}}" height="42" width="42"></td>
+                    </tr>
+                  </table>                
+                </div>
+              </div>
+        </div>
+        <!-- <div class="panel-heading">Hasil Pencarian</div>
         <div class="panel-body">
+        
           <table class="table table-hover table-stripped">
             <tr>
               <td>Judul iklan</td> 
@@ -18,7 +52,7 @@
               <td>Penjual</td>
               <td>Gambar</td>
             </tr>
-            @foreach($iklan as $post)
+            
             <tr>
               <td><a href="{{URL::to('iklan_detail')}}/{{$post->id_iklan}}">{{$post->judul_iklan}}</a></td>
               <td>{{$post->harga}}</td>
@@ -27,10 +61,11 @@
               <td>{{$post->nama_user}}</td>
               <td><img src="{{URL::to($post->gambar)}}" height="42" width="42"></td>
             </tr>
-            @endforeach
+            
           </table>
-        </div>
-      </div>
+          
+        </div> -->
+      @endforeach
     @endif
     </div>
   </div>
